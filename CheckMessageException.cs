0914100b0f806dc57835b5daf2dbeec0a51ad65b
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 17:30:21 +0530

    Inform user if entered Invalid Mood

---
+++ CheckMessageException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class CheckMessageException : Exception
+    {
+        public CheckMessageException(string message) : base(message)
+        {
+
+        }
+    }
+}
