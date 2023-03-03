dddd098242199fc399d6a180168e93b3dc97955e
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 18:51:42 +0530

    Happy Message Using Reflection

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ namespace MoodAnalyzerProblem
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Mood : ");
-            string mood = Console.ReadLine();
-            MoodAnalyzer m1 = new MoodAnalyzer();
-            m1.check(mood);
+            /*   Console.WriteLine("Enter the Mood : ");
+           string mood = Console.ReadLine();
+           MoodAnalyser m1 = new MoodAnalyser();
+           m1.check(mood);
+        */
+
             /*
                 Type t = typeof(MoodAnalyser);
                 ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public|BindingFlags.Instance);
