55ddd455a162c438617328ff221f25ac798f3974
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 17:37:30 +0530

    Given NULL Mood Should Throw MoodAnalysisException

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter the Mood : ");
-                string x = Console.ReadLine();
-                try
-                {
-                    MoodAnalyzer m1 = new MoodAnalyzer();
-                    m1.MoodException(x);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+            string x = Console.ReadLine();
+            try
+            {
+                MoodAnalyzer m1 = new MoodAnalyzer();
+                m1.MoodException(x);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }   
 }
