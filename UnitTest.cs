154194a64bf4d0617533edcb25e1e20c77a55d7a
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 16:22:30 +0530

    analyse and respond happy or sad moods

---
+++ UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoodAnalyzerProblem;
+
+namespace MoodAnalyzerProblem
+{
+        [TestClass]
+        public class UnitTest
+        {
+            [TestMethod]
+            public void Mood_Success()
+            {
+                //Arrange
+                string h = "Sad";
+            MoodAnalyzer m1 = new MoodAnalyzer();
+                string expectedOutput = "SAD";
+
+                //Act 
+                string actualOutput = m1.Mood(h);
+
+            //Assert
+              Assert.AreEqual(expectedOutput, actualOutput);
+
+
+            }
+        }
+}
