18ff2b4d76e23b118bc1e21aeb8a5b0edc113f8f
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 16:22:56 +0530

    analyse and respond happy or sad moods

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        public string Mood(string m)
+        {
+            if (m == "Happy")
+            {
+                return "HAPPY";
+            }
+            else
+            {
+                return "SAD";
+            }
+        }
+    }
+}
