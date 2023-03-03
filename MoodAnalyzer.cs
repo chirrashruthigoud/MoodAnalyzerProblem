2bbf9609609893e185c76d8a34bb80cc02a897e0
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 18:39:29 +0530

    Reflection to invoke Method

--- MoodAnalyzer.cs
+++ MoodAnalyzer.cs
@@ -10,27 +10,8 @@ namespace MoodAnalyzerProblem
     {
         public string check(string mood)
         {
-            if (mood == null)
-            {
-                throw new Exception("No Such Class Error");
-            }
-            /*  else if(mood == "")
-              {
-                  throw new Exception("No Such Method Error");
-              }
-            */
-            else if (mood == "Happy" | mood == "happy")
-            {
-                return "Happy";
-            }
-            else if (mood == "Sad" | mood == "sad")
-            {
-                return "Sad";
-            }
-            else
-            {
-                return mood;
-            }
+            Console.WriteLine("Mood : " + mood);
+
         }
     }
 }
