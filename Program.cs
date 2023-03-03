eef96160531a12b04a2e706bd02031da874b5288
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 17:06:52 +0530

    Given “Handle Exception if User Provides Invalid Mood

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         public static void Main(string[] args)
         {
-            
-        }
+                Program.ErrorFun();
+            }
+
+        public static void ErrorFun()
+        {
+
+            Console.WriteLine("Enter the Mood Happy or Sad: ");
+            string mood = Console.ReadLine();
+            try
+            {
+                if (mood == "Happy")
+                {
+                    Console.WriteLine("Happy");
+                }
+                if (mood == "Sad")
+                {
+                    Console.WriteLine("Sad");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }   
     }
 }
