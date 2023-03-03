b3e6933f1d04508e1f2a8db713514efc1cc290cf
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 18:39:44 +0530

    Reflection to invoke Method

--- Program.cs
+++ Program.cs
@@ -6,25 +6,18 @@ namespace MoodAnalyzerProblem
     {
         public static void Main(string[] args)
         {
-            /* Console.WriteLine("Enter the Mood : ");
+            Console.WriteLine("Enter the Mood : ");
             string mood = Console.ReadLine();
-            MoodAnalyser m1 = new MoodAnalyser(mood);
-            Type t = typeof(MoodAnalyser);
-            ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public|BindingFlags.Instance);
-            foreach (ConstructorInfo info2 in info)
-            {
-                Console.WriteLine(info2.Name);
-            } */
-
-            /* try
-               {
-                   MoodAnalyser m1 = new MoodAnalyser();
-                   m1.check(mood);
-               }
-               catch(Exception e) 
-               {
-                   Console.WriteLine(e.Message);
-               } */
+            MoodAnalyzer m1 = new MoodAnalyzer();
+            m1.check(mood);
+            /*
+                Type t = typeof(MoodAnalyser);
+                ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public|BindingFlags.Instance);
+                foreach (ConstructorInfo info2 in info)
+                {
+                    Console.WriteLine(info2.Name);
+                } 
+            */
 
         }
     }   
--- UnitTest.cs
+++ UnitTest.cs
@@ -11,7 +11,7 @@ namespace MoodAnalyzerProblem
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+     /*   [TestMethod]
         public void MoodAnalyser_Success()
         {
             //Arrange
@@ -25,7 +25,7 @@ namespace MoodAnalyzerProblem
 
             //Assert
             Assert.AreEqual(expectedOutput, actualOutput);
-        }
+        }*/
 
     }
 }
