eaa30de9b72c341ad67893bf215cc65cf8c99917
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 19:09:25 +0530

    Refactor the Code to take message

--- Program.cs
+++ Program.cs
@@ -6,19 +6,9 @@ namespace MoodAnalyzerProblem
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the mood : ");
-            string mood = Console.ReadLine();
-            try
-            {
-                MoodAnalyzer m1 = new MoodAnalyzer();
-                m1.check(mood);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.WriteLine(e.Message);
-            }
+           
+
+            
 
         }
     }   
