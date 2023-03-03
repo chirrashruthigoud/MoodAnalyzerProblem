72575c7beabccd7b60688f5ec158b7d37558c425
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-03 18:21:37 +0530

    Proper Return MoodAnalyser Object

--- UnitTest.cs
+++ UnitTest.cs
@@ -14,26 +14,16 @@ namespace MoodAnalyzerProblem
         [TestMethod]
         public void MoodAnalyser_Success()
         {
-            Console.WriteLine("Enter the Mood : ");
-            string mood = Console.ReadLine();
-            MoodAnalyzer m1 = new MoodAnalyzer(mood);
+            //Arrange
+            MoodAnalyzer m1 = new MoodAnalyzer();
+            MoodAnalyzer m2 = new MoodAnalyzer();
+            object expectedOutput = "Object are Different";
 
-            Type t = typeof(MoodAnalyzer);
-            ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
-            foreach (ConstructorInfo info2 in info)
-            {
-                Console.WriteLine(info2.Name);
-            }
+            //Act 
+            object actualOutput = m1.Equals(m2);
 
-            /* try
-               {
-                   MoodAnalyser m1 = new MoodAnalyser();
-                   m1.check(mood);
-               }
-               catch(Exception e) 
-               {
-                   Console.WriteLine(e.Message);
-               } */
+            //Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
         }
 
     }
